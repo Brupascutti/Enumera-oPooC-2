f7ce9c28dd66dca927f417f4708b249153c9e2a3
Bruno Pascutti <brupascutti@gmail.com>
2024-03-01 11:52:13 -0300

    EnumPooC#

---
+++ ProjeEnumEu/Entitie/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjeEnumEu.Entitie
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Prince { get; set; }
+
+        public Product()
+        { }
+
+        public Product(string name, double prince)
+        {
+            Name = name;
+            Prince = prince;
+        }
+    }
+}
